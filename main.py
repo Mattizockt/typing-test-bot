ccc6a1e9da0f7b737951e88150084fbefa46369c
Matthias Garbers <ggurke2@gmail.com>
2024-03-26 16:22:10 +0800

    added error functionality

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from time import sleep
+import random
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
-def typing_race(type_interval: float):
+def typing_race(type_interval: float, error_probability=0):
     # Create WebDriver instance
     driver = webdriver.Chrome()
     wait = WebDriverWait(driver, timeout=25)
@@ -24,19 +26,30 @@ def typing_race(type_interval: float):
 
         # Read typing text
         text_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//span[@unselectable='on']")))
-        race_text = race_text = ''.join(i.text for i in text_elements[:-1]) + " " + text_elements[-1].text
+        race_text = ''.join(i.text for i in text_elements[:-1]) + " " + text_elements[-1].text
 
         # Find input field
         input_field = driver.find_element(By.XPATH, "//input[@class='txtInput']")
 
-        # Type the text with specified interval
-        for char in race_text: 
-            input_field.send_keys(char)
+        # Type the text with specified interval and error_rate
+        error_counter, i = 0, 0
+        while i < len(race_text):
+            if random.random() < error_probability:
+                input_field.send_keys(chr(ord(race_text[i])+1))
+                error_counter += 1
+            elif error_counter > 0:
+                input_field.send_keys(Keys.BACK_SPACE)
+                error_counter -= 1
+            else:
+                input_field.send_keys(race_text[i])
+                i += 1
             sleep(type_interval)
-    
+
+
     finally:
         # Ensure WebDriver instance is closed
+        sleep(20)
         driver.quit()
 
-# Call the function with specified typing interval
-typing_race(0.08)
+# Call the function with specified typing interval ~ 100 wpm
+typing_race(type_interval=0.08, error_probability=0.1)

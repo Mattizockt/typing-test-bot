8bb08db6403fc06a6437ba032ca29a2d5dba68ec
Matthias Garbers <ggurke2@gmail.com>
2024-04-02 22:39:19 +0800

    another improve demo.py

--- demo.py
+++ demo.py
@@ -3,30 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Initialize WebDriver and WebDriverWait
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, timeout=25)
 
-# go to webpage
+# Navigate to the webpage
 driver.get("https://play.typeracer.com/")
 
-# click the disagree cookies button
+# Click the disagree cookies button
 wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'DISAGREE')]"))).click()
 
-# click the "Enter a Typing Race button"
+# Click the "Enter a Typing Race button"
 wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Enter a Typing Race')]"))).click()
 
-# wait, until the time "remainig element" is visible 
+# Wait until the time "remainig element" is visible 
 wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@title='Time remaining']")))
 
-# find the elements which contain the text which we have to type
+# Find the elements containing the text to type
 text_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//span[@unselectable='on']")))
 
-# merge the text of the elements to a single string
+# Merge the text of the elements to a single string
 race_text = race_text = ''.join(i.text for i in text_elements[:-1]) + " " + text_elements[-1].text
 
 # Find input field
 input_field = driver.find_element(By.XPATH, "//input[@class='txtInput']")
 
-# Type the text with specified interval
+# Type the text 
 for char in race_text: 
     input_field.send_keys(char)

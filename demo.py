d48bd8667aa6c35c163b08cd3f7215d73b769476
Matthias Garbers <ggurke2@gmail.com>
2024-04-03 19:32:37 +0800

    changed comments

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Initialize WebDriver and WebDriverWait
+# Initialize Webdriver and WebDriverWait
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, timeout=25)
 
@@ -13,11 +13,11 @@ driver.get("https://play.typeracer.com/")
 # Click the disagree cookies button
 wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'DISAGREE')]"))).click()
 
-# Click the "Enter a Typing Race button"
+# Click the "Enter a Typing Race" button
 wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Enter a Typing Race')]"))).click()
 
 # Wait until the time "remainig element" is visible 
-wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@title='Time remaining']")))
+wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@title='Time remaining']"))) 
 
 # Find the elements containing the text to type
 text_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//span[@unselectable='on']")))
